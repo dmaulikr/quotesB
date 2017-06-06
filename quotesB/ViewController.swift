6ec9cd6e35ddd474a1c37cb562dc95385ec89c1d
manytime777 <manytime@777.so-net.jp>
2017-06-06 18:40:11 +0900

    Initial Commit

---
+++ quotesB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quotesB
+//
+//  Created by 松岡泰史 on 2017/06/06.
+//  Copyright © 2017年 松岡泰史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
