01a940b5a7dcb0ee71272d2e9d41b3c2106a9ad1
manytime777 <manytime@777.so-net.jp>
2017-06-06 18:57:22 +0900

    Change 2

--- quotesB/ViewController.swift
+++ quotesB/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         //Add some change here
         //Change 1
+        //Change 2
     }
 
     override func didReceiveMemoryWarning() {
