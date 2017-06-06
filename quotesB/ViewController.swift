5dc50eb0e22a0dce5e25db8b6533205f8910f8f1
manytime777 <manytime@777.so-net.jp>
2017-06-06 23:08:29 +0900

    Cleanup project

--- quotesB/ViewController.swift
+++ quotesB/ViewController.swift
@@ -13,15 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //Add some change here
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    //I am developer B and I create a lot of code on the ViewController
 
 }
 
