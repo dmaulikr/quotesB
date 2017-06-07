b6e4e0606b499924167171c5032a9101eb3237c8
manytime777 <manytime@777.so-net.jp>
2017-06-07 11:49:05 +0900

    Start implement button to display quotes

--- quotesB/ViewController.swift
+++ quotesB/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func displayNewQuotes(_ sender: Any) {
+        print("Press \"Display New Quotes\"")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
